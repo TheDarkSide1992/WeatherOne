084c35a94e5356ed372a629c286b214e17643171
TheDarkSide1992 <Jenscp171@gmail.com>
2024-03-14 09:41:17 +0100

    The Location update (Works on web) Not yet on android

--- lib/data_source.dart
+++ lib/data_source.dart
@@ -2,6 +2,7 @@ import 'dart:convert';
 
 import 'package:flutter/services.dart';
 import 'package:http/http.dart' as http;
+import 'package:location/location.dart';
 import 'models.dart';
 import 'models/time_series.dart';
 
@@ -33,27 +34,60 @@ class FakeDataSource implements DataSource {
 }
 
 class RealDataSource implements DataSource {
+  var latitude = 55.4703;
+  var longitude = 8.4519;
 
   @override
   Future<WeeklyForecastDto> getWeeklyForecast() async {
+    try {
+      final location = await Location.instance.getLocation();
+      if (location.latitude != null) {
+        this.latitude = location.latitude!;
+        this.longitude = location.longitude!;
+      }
+    } catch (error){
+      var latitude = 55.4703;
+      var longitude = 8.4519;
+    }
 
-    final response = await http.get(Uri.parse("https://api.open-meteo.com/v1/forecast?latitude=55.4703&longitude=8.4519&daily=weather_code,temperature_2m_max,temperature_2m_min"));
+    final response = await http.get(Uri.parse("https://api.open-meteo.com/v1/forecast?latitude=$latitude&longitude=$longitude&daily=weather_code,temperature_2m_max,temperature_2m_min"));
     final json = response.body;
     return WeeklyForecastDto.fromJson(jsonDecode(json));
   }
 
   @override
   Future<WeatherChartData> getChartData() async {
+    try {
+      final location = await Location.instance.getLocation();
+      if (location.latitude != null) {
+        this.latitude = location.latitude!;
+        this.longitude = location.longitude!;
+      }
+    } catch (error){
+      var latitude = 55.4703;
+      var longitude = 8.4519;
+    }
 
-    final response = await http.get(Uri.parse("https://api.open-meteo.com/v1/forecast?latitude=55.4703&longitude=8.4519&hourly=temperature_2m&daily=temperature_2m_max,temperature_2m_min"));
+    final response = await http.get(Uri.parse("https://api.open-meteo.com/v1/forecast?latitude=$latitude&longitude=$longitude&hourly=temperature_2m&daily=temperature_2m_max,temperature_2m_min"));
     return WeatherChartData.fromJson(jsonDecode(response.body));
   }
 
   @override
   Future<WeatherChartData> getChartDataHour(DateTime date) async {
+    try {
+      final location = await Location.instance.getLocation();
+      if (location.latitude != null) {
+        this.latitude = location.latitude!;
+        this.longitude = location.longitude!;
+      }
+    } catch (error){
+      var latitude = 55.4703;
+      var longitude = 8.4519;
+    }
+
     final startTime = date.toIso8601String().split("T")[0];
     final endTime = date.add(Duration(days: 1)).toIso8601String().split("T")[0];
-    final response = await http.get(Uri.parse("https://api.open-meteo.com/v1/forecast?latitude=55.4703&longitude=8.4519&hourly=temperature_2m&start_date=$startTime&end_date=$endTime"));
+    final response = await http.get(Uri.parse("https://api.open-meteo.com/v1/forecast?latitude=$latitude&longitude=$longitude&hourly=temperature_2m&start_date=$startTime&end_date=$endTime"));
     return WeatherChartData.fromJson(jsonDecode(response.body));
   }
 }
